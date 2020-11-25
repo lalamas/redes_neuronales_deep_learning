43328bc921fb211fbae0d00e2b0607dc1e83c561
lalamas <36405516+lalamas@users.noreply.github.com>
2020-11-26 00:06:03 +0100

    Update README.cmd

--- README.cmd
+++ README.cmd
@@ -1,1 +1,10 @@
 Proyecto de redes neuronales y deep learning
+============================================
+1.-Conocimientos de Deep Learning
+2.-Básico de Redes Neuronales
+3.-Regresión Logistico con Redes Neuronales
+4.-Redes neuronales poco profundas
+5.-Clasificacion de datos con una capa oculta
+6.-Conceptos claves de redes Deep Learning
+7.-Creación de una red neuronal paso a pado
+8.-Definicion de una aplicación
